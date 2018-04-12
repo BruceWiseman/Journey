40620ade7fa5e858518ae9a25fa2369c9607e110
BARTLOMIEJ GODON (1604798) <1604798@rgu.ac.uk>
2018-04-12 09:36:47 +0100

    stuff

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 // index page
 app.get('/', function(req, res) {
- res.render('pages/homePage');
+ res.render('pages/home');
 });
 
 app.listen(8080);
