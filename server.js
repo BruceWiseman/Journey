61c93f93cdb4536f5eacb5b4bd470cb74987e9d2
barte <bartekgodon@yahoo.co.uk>
2018-04-14 13:38:20 +0100

    fix?

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/profile', function(req, res) {
   db.collection('people').findOne({"login.username": uname}, function(err, result) {
     if (err) throw err;
 
-    res.render('pages/profile', {
+    res.render('pages/profile?username=<%= user.login.username %>', {
       user:result
     })
   });
