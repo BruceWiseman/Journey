ae421d83ba1530b34daca7d68e25899aa48cd385
barte <bartekgodon@yahoo.co.uk>
2018-04-14 11:30:57 +0100

    register test

--- server.js
+++ server.js
@@ -55,31 +55,21 @@ app.get('/profile', function(req, res) {
 
 app.post('/adduser', function(req, res) {
   //check we are logged in
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if(!req.session.loggedin){res.redirect('/');return;}
 
   //we create the data string from the form components that have been passed in
 
 var datatostore = {
-"gender":req.body.gender,
-"name":{"title":req.body.title,"first":req.body.first,"last":req.body.last},
-"location":{"street":req.body.street,"city":req.body.city,"state":req.body.state,"postcode":req.body.postcode},
-"email":req.body.email,
-"login":{"username":req.body.username,"password":req.body.password},
-"dob":req.body.dob,"registered":Date(),
-"picture":{"large":req.body.large,"medium":req.body.medium,"thumbnail":req.body.thumbnail},
-"nat":req.body.nat}
+  "name":{"first":req.body.first,"last":req.body.last},
+  "email":req.body.email,
+  "login":{"username":req.body.email"password":req.body.psw}};
 
 
 //once created we just run the data string against the database and all our new data will be saved/
-  db.collection('people').save(datatostore, function(err, result) {
-    if (err) throw err;
-    console.log('saved to database')
+  //db.collection('people').save(datatostore, function(err, result) {
+    //if (err) throw err;
+    console.log(datastore);
     //when complete redirect to the index
-    res.redirect('/')
+    //res.redirect('/')
   })
 });
-
-
-// listen on port
-// app.listen(8080);
-// console.log('8080 is the magic port');
