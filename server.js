30cdc9d7580d890c435ba7b0a01cf509bca07d01
barte <bartekgodon@yahoo.co.uk>
2018-04-14 12:27:29 +0100

    login test

--- server.js
+++ server.js
@@ -73,3 +73,23 @@ var datatostore = {
     res.redirect('/')
   })
 });
+
+//Login user
+
+//the dologin route deals with the data from the login screen.
+//the post variables, username and password come from the login form
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var uname = req.body.username;
+  var pword = req.body.password;
+
+  db.collection('people').findOne({"login.username":uname}, function(err, result) {
+    if (err) throw err;//if there is an error, throw the error
+    //if there is no result, redirect the user back to the login system as that username must not exist
+    if(!result){res.redirect('/');return}
+    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/profile') }
+    //otherwise send them back to login
+    else{res.redirect('/')}
+  });
+});
